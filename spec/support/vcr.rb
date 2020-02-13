12d3c7b23f741ee3f89195b3807409e2498452dc
Marcelo Bohn <marcelo.bohn@gmail.com>
2020-02-13 10:42:38 -0300

    fix spec of fake twitter resquests

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,8 +3,12 @@ VCR.configure do |c|
   c.default_cassette_options = {
     match_requests_on: %i[method uri body],
     serialize_with: :json,
-    # serialize_with: :yaml,
     record: :new_episodes
   }
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :faraday
+
+  c.before_record do |i|
+    i.response.body.force_encoding('UTF-8')
+  end
 end
