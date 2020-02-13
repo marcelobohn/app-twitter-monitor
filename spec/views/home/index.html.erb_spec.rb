2513653ae2a24828739b26f5aeb4839d13291051
Marcelo Bohn <marcelo.bohn@gmail.com>
2020-02-13 10:45:08 -0300

    fix spec of home view

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'home/index.html.erb', type: :view do
   it 'displays default text' do
+    @hashtags = {}
+
     render
 
     expect(rendered).to match('Twitter monitor')
