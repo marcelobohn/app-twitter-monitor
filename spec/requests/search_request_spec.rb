143fac002fc7c297adfc6d1a6d7154891a2c7d7f
Marcelo Bohn <marcelo.bohn@gmail.com>
2020-02-13 10:47:35 -0300

    add vcr cassette in spec

--- spec/requests/search_request_spec.rb
+++ spec/requests/search_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Searches", type: :request do
   describe "GET /:term" do
-    it "returns http success" do
+    it "returns http success", vcr: { cassette_name: 'fake_twitter_api_facebook' }  do
       get "/search/brazil"
       expect(response).to have_http_status(:success)
     end
